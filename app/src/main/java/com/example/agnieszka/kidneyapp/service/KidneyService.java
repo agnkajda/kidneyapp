567932dd76f0c5c0c0b094a9ad1279412eb9938f
Agnieszka <agnkajda@student.agh.edu.pl>
2016-09-28 02:43:48 +0200

    Nowe pliki

---
+++ app/src/main/java/com/example/agnieszka/kidneyapp/service/KidneyService.java
@@ -0,0 +1,19 @@
+package com.example.agnieszka.kidneyapp.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Agnieszka on 28.09.2016.
+ */
+
+public class KidneyService extends IntentService {
+    public KidneyService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
